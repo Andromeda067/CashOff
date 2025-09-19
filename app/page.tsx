da07b8e274664733fe56fe25b8fba5e93c07a985
Andromeda067 <gui.motta6@gmail.com>
2025-09-19 10:46:28 -0400

    add: interactionb text with my mon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-export default function Home () {
-  return(
-    <h1>Hello world!!!!</h1>
+export default function Home() {
+  return (
+    <h1 className="text-4xl text-green-500 flex flex-1 items-center f">
+      Fique totalmente seria se eu posso ter uma moto sem ningem me encher o saco  🏍️🏍️🏍️🏍️🏍️🏍️🏍️🏍️🏍️🏍️🏍️🏍️!!!!
+    </h1>
+
   )
 }
